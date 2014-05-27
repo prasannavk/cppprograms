6423ccd0d02495df41e9cdcd47473136490ecd0a
prasannavk <pras.bits@gmail.com>
2014-05-27 08:50:48 -0700

    Update Permutations.cpp
    indenting fixes

--- Permutations.cpp
+++ Permutations.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 void Permute(int* arr, int* permutation, const int n)
 {
-
     if( n == 0)
     {
     	int i = 5;// FIXME: HARD-CODED
@@ -18,25 +17,26 @@ void Permute(int* arr, int* permutation, const int n)
     	}
     	cout << permutation[i] << "\n";
     }
-	for(int i=0; i<n; i++)
-	{
-		//cout << "(" << i<< "," << n << ")" << arr[i] << ",";
-		permutation[n-1] = arr[i];
-		int temp = arr[0];
-		arr[0] = arr[i];
-		arr[i] = temp;
-		Permute(arr+1, permutation, n-1);
-		temp = arr[i];
-		arr[i] = arr[0];
-		arr[0] = temp;
-	}
+    
+    for(int i=0; i<n; i++)
+    {
+       //cout << "(" << i<< "," << n << ")" << arr[i] << ",";
+       permutation[n-1] = arr[i];
+       int temp = arr[0];
+       arr[0] = arr[i];
+       arr[i] = temp;
+       Permute(arr+1, permutation, n-1);
+       temp = arr[i];
+       arr[i] = arr[0];
+       arr[0] = temp;
+    }
 }
 
 int main()
 {
-	int array[] = {1,2,3,4,5,6};
+    int array[] = {1,2,3,4,5,6};
     const int SIZE = 6;
     int* permutation = new int[SIZE];
     Permute(array, permutation, SIZE);
-	return 0;
+    return 0;
 }
