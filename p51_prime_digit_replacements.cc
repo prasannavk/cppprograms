d463f876f2d9e4caf620dec140dfe2b0bf3649ea
Prasanna Krishnasamy <pras.bits@gmail.com>
2016-04-17 00:08:50 -0700

    updates to p51

--- p51_prime_digit_replacements.cc
+++ p51_prime_digit_replacements.cc
@@ -10,6 +10,16 @@ using std::vector;
 std::vector<long long int> prime_numbers;
 std::vector<long long int> other_prime_list;
 
+// this is the prime number family we are looking for. It has to be of size 8.
+struct PrimeNumberFamily {
+  int size;
+  vector<int> index_list;
+  vector<long long int> prime_family_list;
+};
+
+// a canon number is the repeated digits replaced with 1
+std::map<int, PrimeNumberFamily> canon_number_to_family;
+
 // this is the map from index numbers of a k-digit number to a list of prime
 // numbers that have the same indexes repeated
 std::map<int, vector<long long int> > indexes_to_prime_numbers;
@@ -46,16 +56,20 @@ int TestPrime(int n) {
 }
 
 // index_list for say 83647 is 54321 - it's the digit's place in a crazy ad-hoc format
-bool IsDigitRepeating(long long int prime_number, std::map<int, Metadata> *digit_map) { 
-  bool output = false;
+bool IsDigitRepeating(long long int prime_number, std::map<int, Metadata> *digit_map, long long int *canon_number) { 
+  bool is_repeated = false;
   int digit_index = 1;
+  canon_number = 0;
   while (prime_number !=0) {
     int digit  = prime_number % 10;
     if (digit_map->find(digit) != digit_map->end()) {
-      output = true;
+      is_repeated = true;
       Metadata &digit_meta = (*digit_map)[digit];
       digit_meta.repeating_digit = digit;
       digit_meta.index_list.push_back(digit_index);
+
+      // TODO: Need to handle 2 different digits repeating
+      canon_number = canon_number*10 + 1;
     } else {
       Metadata digit_meta;
       digit_meta.repeating_digit = digit;
@@ -63,11 +77,13 @@ bool IsDigitRepeating(long long int prime_number, std::map<int, Metadata> *digit
       index_list_local.push_back(digit_index);
       digit_meta.index_list = index_list_local;
       (*digit_map)[digit] = digit_meta;
+
+      canon_number = canon_number*10 + digit;
     }
     prime_number /= 10;
     digit_index++;
   }
-  return output;
+  return is_repeated;
 }
 
 int main() {
@@ -87,7 +103,8 @@ int main() {
       if (current_num > begin_number && current_num < end_number) {
         other_prime_list.push_back(current_num);
         std::map<int, Metadata> digit_map;
-        if (IsDigitRepeating(current_num, &digit_map)) {
+        long long int canon_number = 0;
+        if (IsDigitRepeating(current_num, &digit_map, &canon_number)) {
           cout << current_num << "\n";
           // There could be 2 digits repeating
           for (auto& kv : digit_map) {
@@ -108,6 +125,16 @@ int main() {
               std::vector<long long int> &prime_num_vec = indexes_to_prime_numbers[indexes_as_single_num];
               prime_num_vec.push_back(current_num);
             }
+
+            // make the map for canon number
+            if (canon_number_to_family.find(canon_number) == canon_number_to_family.end()) {
+              PrimeNumberFamily pfamily;
+
+              canon_number_to_family[canon_number] = temp;
+            } else {
+            }
+
+
           }
         }
       }
@@ -117,11 +144,13 @@ int main() {
   cout <<"-------------------------\n";
   for (auto kv : indexes_to_prime_numbers) {
     cout << kv.first << " has prime_number list size " << kv.second.size() << "\n";
+#if 0
     if (kv.first == 23) {
       cout << "\n";
       copy(kv.second.begin(), kv.second.end(), std::ostream_iterator<int>(cout, ","));
       cout << "\n"; 
     }
+#endif
   }
    
 
